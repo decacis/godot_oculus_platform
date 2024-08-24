c906224d6780eeed0de92243043a7dc45439b939
Daniel Castellanos <decacis@gmail.com>
2024-08-23 22:44:36 -0600

    Upgrading SDK to v67.0

--- src/include/OVR_AchievementProgress.h
+++ src/include/OVR_AchievementProgress.h
@@ -8,10 +8,21 @@
 
 typedef struct ovrAchievementProgress *ovrAchievementProgressHandle;
 
-OVRP_PUBLIC_FUNCTION(const char *)       ovr_AchievementProgress_GetBitfield(const ovrAchievementProgressHandle obj);
+/// For bitfield achievements, the current bitfield state.
+OVRP_PUBLIC_FUNCTION(const char *) ovr_AchievementProgress_GetBitfield(const ovrAchievementProgressHandle obj);
+
+/// For count achievements, the current counter state.
 OVRP_PUBLIC_FUNCTION(unsigned long long) ovr_AchievementProgress_GetCount(const ovrAchievementProgressHandle obj);
-OVRP_PUBLIC_FUNCTION(bool)               ovr_AchievementProgress_GetIsUnlocked(const ovrAchievementProgressHandle obj);
-OVRP_PUBLIC_FUNCTION(const char *)       ovr_AchievementProgress_GetName(const ovrAchievementProgressHandle obj);
+
+/// If the user has already unlocked this achievement.
+OVRP_PUBLIC_FUNCTION(bool) ovr_AchievementProgress_GetIsUnlocked(const ovrAchievementProgressHandle obj);
+
+/// The unique string that you use to reference the achievement in your app, as
+/// specified in the developer dashboard.
+OVRP_PUBLIC_FUNCTION(const char *) ovr_AchievementProgress_GetName(const ovrAchievementProgressHandle obj);
+
+/// If the achievement is unlocked, the time when it was unlocked.
 OVRP_PUBLIC_FUNCTION(unsigned long long) ovr_AchievementProgress_GetUnlockTime(const ovrAchievementProgressHandle obj);
+
 
 #endif
--- src/include/OVR_ApplicationVersion.h
+++ src/include/OVR_ApplicationVersion.h
@@ -7,15 +7,27 @@
 
 typedef struct ovrApplicationVersion *ovrApplicationVersionHandle;
 
-/// Seconds since epoch when the latest app update was released
+/// Version code number for the version of the application currently installed
+/// on the device.
+OVRP_PUBLIC_FUNCTION(int) ovr_ApplicationVersion_GetCurrentCode(const ovrApplicationVersionHandle obj);
+
+/// Version name string for the version of the application currently installed
+/// on the device.
+OVRP_PUBLIC_FUNCTION(const char *) ovr_ApplicationVersion_GetCurrentName(const ovrApplicationVersionHandle obj);
+
+/// Version code number of the latest update of the application. This may or
+/// may not be currently installed on the device.
+OVRP_PUBLIC_FUNCTION(int) ovr_ApplicationVersion_GetLatestCode(const ovrApplicationVersionHandle obj);
+
+/// Version name string of the latest update of the application. This may or
+/// may not be currently installed on the device.
+OVRP_PUBLIC_FUNCTION(const char *) ovr_ApplicationVersion_GetLatestName(const ovrApplicationVersionHandle obj);
+
+/// Seconds since epoch when the latest application update was released.
 OVRP_PUBLIC_FUNCTION(long long) ovr_ApplicationVersion_GetReleaseDate(const ovrApplicationVersionHandle obj);
 
-/// Size of the app update in bytes
+/// Size of the latest application update in bytes.
 OVRP_PUBLIC_FUNCTION(const char *) ovr_ApplicationVersion_GetSize(const ovrApplicationVersionHandle obj);
 
-OVRP_PUBLIC_FUNCTION(int)          ovr_ApplicationVersion_GetCurrentCode(const ovrApplicationVersionHandle obj);
-OVRP_PUBLIC_FUNCTION(const char *) ovr_ApplicationVersion_GetCurrentName(const ovrApplicationVersionHandle obj);
-OVRP_PUBLIC_FUNCTION(int)          ovr_ApplicationVersion_GetLatestCode(const ovrApplicationVersionHandle obj);
-OVRP_PUBLIC_FUNCTION(const char *) ovr_ApplicationVersion_GetLatestName(const ovrApplicationVersionHandle obj);
 
 #endif
